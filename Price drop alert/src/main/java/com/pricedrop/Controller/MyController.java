6eda494c9d1bd22d07a9223eaa738b616eceee51
jain-18 <shubhamjain12360@gmail.com>
2024-03-02 13:14:18 +0530

    product form added with security

--- Price drop alert/src/main/java/com/pricedrop/Controller/MyController.java
+++ Price drop alert/src/main/java/com/pricedrop/Controller/MyController.java
@@ -6,6 +6,8 @@ import com.pricedrop.helper.Message;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyController {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @RequestMapping("/")
     public String Home(){
@@ -28,8 +33,11 @@ public class MyController {
     }
 
     @PostMapping("/do_register")
-    public String registerUser(@ModelAttribute User user, HttpSession session){
+    public String registerUser(@ModelAttribute("user") User user, HttpSession session){
         user.setRole("ROLE_USER");
+        System.out.println(user.getPassword());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        System.out.println(user);
         this.userRepository.save(user);
         session.setAttribute("message", new Message("Successfully Registered!!", "alert-success"));
         return "redirect:/signup";
--- Price drop alert/src/main/java/com/pricedrop/Controller/UserController.java
+++ Price drop alert/src/main/java/com/pricedrop/Controller/UserController.java
@@ -7,9 +7,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @RequestMapping("/dashboard")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public String Dashboard(){
+        return "dashboard";
+    }
     @RequestMapping("/add-product")
     @PreAuthorize("hasAuthority('ROLE_USER')")
     public String product(){
         return "add_product";
+    }
+
+    @RequestMapping("/added_product")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public String AddedProduct(){
+        return "redirect:/user/add-product";
     }
 }
