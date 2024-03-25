da8736810d514faaea58b1f8e07bc2478ebff516
jain-18 <shubhamjain12360@gmail.com>
2024-03-25 10:42:52 +0530

    done with validation in register form

--- Price drop alert/src/main/java/com/pricedrop/Controller/MyController.java
+++ Price drop alert/src/main/java/com/pricedrop/Controller/MyController.java
@@ -11,11 +11,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
+import java.security.Principal;
 import java.util.Random;
 
 @Controller
@@ -31,8 +34,13 @@ public class MyController {
     private EmailService emailService;
 
     @RequestMapping("/")
-    public String Home(){
-        return "home1";
+    public String Home(Principal principal){
+        if(principal!=null){
+            return "redirect:/user/dashboard";
+        }
+        else {
+            return "home1";
+        }
     }
 
     @RequestMapping("/about")
@@ -41,7 +49,8 @@ public class MyController {
     }
 
     @RequestMapping("/signup")
-    public String SignUp(){
+    public String SignUp(Model model){
+        model.addAttribute("user",new User());
         return "signup1";
     }
 
@@ -51,53 +60,82 @@ public class MyController {
     }
 
     @PostMapping("/do_register")
-    public String registerUser(@ModelAttribute("user") User user, HttpSession session){
-        user.setRole("ROLE_USER");
-        System.out.println(user.getPassword());
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        System.out.println(user);
-        this.userRepository.save(user);
-        session.setAttribute("message", new Message("Successfully Registered!!", "alert-success"));
-        return "redirect:/signup";
-    }
-
-    @PostMapping("/sendOTP")
-    public String verifyUser(@ModelAttribute("user") User user, HttpSession session, Model model){
-        Random random = new Random();
-        model.addAttribute("title","Email");
-        //generating otp of 4 digit
-        int otp = random.nextInt(9999999);
-        System.out.println(user.getEmail());
-        System.out.println(otp);
-
-        //write a code for send otp to email
-        String subject = "OTP for verification of email from price Drop Alert";
-        String message = "" +
-                "<div style='border:1px solid #e2e2e2: padding:20px'>" +
-                "<h1>" +
-                "OTP is " +
-                "<b>" + otp +
-                "</n>" +
-                "</h1>" +
-                "</div>";
-        String to = user.getEmail();
-
-//        boolean flag = this.emailService.sendEmail(subject,message,to);
-        boolean flag = true;
-        if(flag){
-
-            session.setAttribute("user",user);
-            session.setAttribute("myotp",otp);
-            session.setAttribute("email",user.getEmail());
-            session.setAttribute("message",new Message("We have sent OTP to your email.."," alert-success "));
-
-            return "verify_otp";
-        }
-        else{
-            session.setAttribute("message",new Message("Enter your correct email-Id!!"," alert-danger "));
+    public String registerUser(@Valid @ModelAttribute("user") User user, HttpSession session, BindingResult bindingResult){
+        try{
+            User user1 = this.userRepository.getUserByUserName(user.getEmail());
+            if(user1 == null){
+                user.setRole("ROLE_USER");
+                System.out.println(user.getPassword());
+                user.setPassword(passwordEncoder.encode(user.getPassword()));
+                System.out.println(user);
+                this.userRepository.save(user);
+                session.setAttribute("message", new Message("Successfully Registered!!", "alert-success"));
+                return "redirect:/signup";
+            }
+            else{
+                session.setAttribute("message", new Message("Email-Id Already Exist!!", "alert-danger"));
+                session.setAttribute("user",user);
+                return "redirect:/signup";
+            }
+        }catch (Exception e){
+            session.setAttribute("message", new Message("Something went wrong!!", "alert-danger"));
             return "signup";
         }
 
+    }
+
+    @PostMapping("/sendOTP")
+    public String verifyUser(@Valid @ModelAttribute("user") User user,BindingResult result, HttpSession session, Model model){
+        try{
+            User user1 = this.userRepository.getUserByUserName(user.getEmail());
+            if(result.hasErrors()){
+                model.addAttribute("user", user);
+                return "signup1";
+            }
+            if(user1 == null){
+                Random random = new Random();
+                model.addAttribute("title","Email");
+                //generating otp of 4 digit
+                int otp = random.nextInt(9999999);
+                System.out.println(user.getEmail());
+                System.out.println(otp);
+
+                //write a code for send otp to email
+                String subject = "OTP for verification of email from price Drop Alert";
+                String message = "" +
+                        "<div style='border:1px solid #e2e2e2: padding:20px'>" +
+                        "<h1>" +
+                        "OTP is " +
+                        "<b>" + otp +
+                        "</n>" +
+                        "</h1>" +
+                        "</div>";
+                String to = user.getEmail();
+
+//        boolean flag = this.emailService.sendEmail(subject,message,to);
+                boolean flag = true;
+                if(flag){
+
+                    session.setAttribute("user",user);
+                    session.setAttribute("myotp",otp);
+                    session.setAttribute("email",user.getEmail());
+                    session.setAttribute("message",new Message("We have sent OTP to your email.."," alert-success "));
+                    return "verify_otp";
+                }
+                else{
+                    session.setAttribute("message",new Message("Enter your correct email-Id!!"," alert-danger "));
+                    return "signup";
+                }
+            }
+            else{
+                session.setAttribute("message", new Message("Email-Id Already Exist!!", "alert-danger"));
+                session.setAttribute("user",user);
+                return "redirect:/signup";
+            }
+        }catch (Exception e){
+            session.setAttribute("message", new Message("Something went wrong!!", "alert-danger"));
+            return "signup";
+        }
     }
 
     @PostMapping("/verify-otp")
--- Price drop alert/src/main/java/com/pricedrop/entities/User.java
+++ Price drop alert/src/main/java/com/pricedrop/entities/User.java
@@ -1,6 +1,9 @@
 package com.pricedrop.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,11 +18,20 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int u_id;
+    @NotBlank(message = "Name field is required!!")
+    @Size(min =2,max=30,message = "Min 2 and Max 30 Character are allowed")
     private String username;
+
+    @Column(unique = true)
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",message = "Invalid email")
     private String email;
+
+    @Size(min = 4, message = "Password must be at least 4 characters long")
     private String password;
     private String role;
-    private long mno;
+
+    @Pattern(regexp = "^\\d{8,10}$", message = "Invalid mobile number. Please enter a 10-digit number.")
+    private String mno;
 
     @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER,mappedBy = "user")
     private List<Product> product = new ArrayList<>();
